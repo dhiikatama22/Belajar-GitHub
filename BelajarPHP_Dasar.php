0015f7d97711896a2492642dec4916bac2789761
dhiikatama <dhiikatama@hotmail.com>
2020-04-26 07:49:53 +0700

    Update Favorite Number

--- BelajarPHP_Dasar.php
+++ BelajarPHP_Dasar.php
@@ -2,12 +2,15 @@
     for ( $i=1; $i<=10; $i++ ){
         echo "<br>Perulangan ke-",$i;
         
-        if ($i == 7) {
-            echo " = Binggo";   
+        if ($i == 3) {
+                echo " = Binggo";   
+
+            } else if ($i == 7){
+                echo " = That's My Favorite Number";
 
             } else if ($i == 9){
-            echo " = That's my Birthday Date";
-        
+                echo " = That's my Birthday Date";
+
         } else {
  
         }
